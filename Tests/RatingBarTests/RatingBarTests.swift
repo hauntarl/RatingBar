b7007065c872da347a89d5a72321eaca814aa8d9
hauntarl <sameer.mungole@gmail.com>
2024-01-23 05:06:34 -0700

    Initial Commit

---
+++ Tests/RatingBarTests/RatingBarTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import RatingBar
+
+final class RatingBarTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
