5419c26a86adaf73cab5b114f1ffc8327b3974de
hauntarl <sameer.mungole@gmail.com>
2024-01-23 20:59:01 -0700

    Add hexagon shape

--- Sources/RatingBar/CustomShapes.swift
+++ Sources/RatingBar/CustomShapes.swift
@@ -36,6 +36,55 @@ public struct RatingBarTriangle: Shape {
     }
 }
 
+// Source - https://sarunw.com/posts/how-to-draw-custom-paths-and-shapes-in-swiftui/
+struct RatingBarHexagon: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let width: CGFloat = rect.width
+            let height: CGFloat = rect.height
+            
+            path.move(
+                to: CGPoint(
+                    x: 0.2 * width,
+                    y: 0 * height
+                )
+            )
+
+            path.addLine(
+                to: CGPoint(
+                    x: 0.8 * width,
+                    y: 0 * height)
+            )
+
+            path.addLine(
+                to: CGPoint(
+                    x: 1 * width,
+                    y: 0.5 * height)
+            )
+            
+            path.addLine(
+                to: CGPoint(
+                    x: 0.8 * width,
+                    y: 1 * height)
+            )
+            
+            path.addLine(
+                to: CGPoint(
+                    x: 0.2 * width,
+                    y: 1 * height)
+            )
+            
+            path.addLine(
+                to: CGPoint(
+                    x: 0 * width,
+                    y: 0.5 * height)
+            )
+
+            path.closeSubpath()
+        }
+    }
+}
+
 
 // Source - https://stackoverflow.com/a/63650217
 public struct RatingBarRoundedStar: Shape {
